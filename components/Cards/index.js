6994254055840d87e1169b652edef7e6159ce007
smv5047 <smv5047@gmail.com>
2019-08-18 20:49:56 -0400

    completed cards portion

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,69 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//Card Component
+
+function cardCreator (array) {
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+    let headline = document.createElement("div")
+    headline.classList.add("headline")
+    //Update Reference
+    headline.textContent = array.headline
+    card.appendChild(headline)
+
+    let author = document.createElement("div")
+    author.classList.add("author")
+
+    let imgContainer = document.createElement("div")
+    imgContainer.classList.add("img-container")
+    
+    let imgAuthor = document.createElement("img")
+    //Update Reference
+    imgAuthor.src = array.authorPhoto
+    imgContainer.appendChild(imgAuthor)
+
+    let authorName = document.createElement("span")
+    //Update Reference
+    authorName.textContent = array.authorName
+
+    author.appendChild(imgContainer)
+    author.appendChild(authorName)
+
+    card.appendChild(author)
+
+
+    return card
+
+}
+
+let cardsContainer = document.querySelector(".cards-container")
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(resolve => {
+        let articles = {};
+        let articlesTitles = [];
+        articles = resolve.data.articles
+        articlesTitles = Object.values(articles)
+        // console.log(articlesTitles[0])
+        let arrayOfArticles = []
+        for (let i =0; i<articlesTitles.length; i++) {
+            for (let x=0; x<articlesTitles[i].length; x++){
+                arrayOfArticles.push(articlesTitles[i][x])
+            }
+        }
+        arrayOfArticles.forEach(article => {
+            cardsContainer.appendChild(cardCreator(article))
+        })
+       
+        // console.log(arrayOfArticles)
+        // console.log(resolve)
+        // console.log(articlesTitles)
+    })
+    .catch( error => {
+        console.log(error)
+    })
+
+
